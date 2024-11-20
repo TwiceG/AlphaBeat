eaad14c71642be55ec95371d1c3f4d63aa231a6e
TwiceG <g0stron0g@gmail.com>
2024-11-21 00:47:02 +0100

    adjust import

--- src/components/Highscores.tsx
+++ src/components/Highscores.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import supabase from '../SupaBaseClient';
-import { Highscore } from '../types/Highscore';
+import { Highscore } from '../types/highscore';
 import '../style/Highscores.css';
 
 const Highscores: React.FC = () => {
